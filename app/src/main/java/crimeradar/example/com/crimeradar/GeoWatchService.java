75f379a0b66afd87b5f37e7e2e9e653c399c46d1
Veselin <kirev@outlook.com>
2015-04-12 02:35:08 +0100

    Service gps popup ready to test

--- app/src/main/java/crimeradar/example/com/crimeradar/GeoWatchService.java
+++ app/src/main/java/crimeradar/example/com/crimeradar/GeoWatchService.java
@@ -2,21 +2,60 @@ package crimeradar.example.com.crimeradar;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
 
-public class GeoWatchService extends Service {
+public class GeoWatchService extends Service  implements LocationListener {
     public GeoWatchService() {
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         //TODO do something useful
+        LocationManager locService = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enabled = locService.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if(!enabled)
+        {
+            intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
         return Service.START_STICKY;
     }
 
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        int lat = (int) (location.getLatitude());
+        int lng = (int) (location.getLongitude());
+        Log.i("Test latitude: ",String.valueOf(lat));
+        Log.i("Test longitude: ", String.valueOf(lng));
+    }
     @Override
     public IBinder onBind(Intent intent) {
         //TODO for communication return IBinder implementation
         return null;
     }
+
 }
